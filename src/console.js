561935b5689cbd8687302410f787ecffbd574f29
unknown <me@rossmers.ch>
2022-10-29 16:36:50 -0400

    Add .JS for deployment functionality. Fixes some relative paths.

---
+++ src/console.js
@@ -0,0 +1,4 @@
+  // Creditary Material.
+
+  console.log('%c Designed by Vasek, Help from Ross M. \n Hosted by: RLM Technologies.', 'color: red; font-size: 12px;');
+  console.log(' ');
--- src/meta.js
+++
@@ -1,3 +0,0 @@
-  // Creditary Material.
-
-console.log('%c Designed by Vasek, Help from Ross M. \n Hosted by: RLM Technologies.', 'color: red; font-size: 12px;');
