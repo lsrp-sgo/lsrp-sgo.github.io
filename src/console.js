55201e5d933ec6c1458054e6df6cdf07a37ea8bd
unknown <me@rossmers.ch>
2022-10-29 16:51:04 -0400

    Fixed some broken stuff.

--- src/console.js
+++ src/console.js
@@ -1,4 +1,4 @@
   // Creditary Material.
-
-  console.log('%c Designed by Vasek, Help from Ross M. \n Hosted by: RLM Technologies.', 'color: red; font-size: 12px;');
+  console.log('%c 👀 Site made w/ Vasek. I invision creating a service that caters to all and allows ease-of-use with gathering knowledge. You may contact me (the hostmaster) at webmaster.lsrp@rossmers.ch.', 'color: red; font-size: 12px;');
+  console.log('%c ', 'color: red; font-size: 12px;');
   console.log(' ');
