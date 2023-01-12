96ab29bfd92d64e09a66f9757ab4278362a34473
Ross <me@rossmers.ch>
2023-01-11 22:33:59 -0500

    Fixed some pages

--- update.sh
+++ update.sh
@@ -1,5 +1,1 @@
-sudo git config --global --add safe.directory /home/government/public_html/
-&&
-sudo git pull
-&&
-sudo chown -v government:government /home/government/public_html/
+sudo git config --global --add safe.directory /home/government/public_html/ && sudo git pull && sudo chown -v government:government /home/government/public_html/
